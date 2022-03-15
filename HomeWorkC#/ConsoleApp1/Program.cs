9e986fb462a8f53248b355e1ca75f6a139437c15
Resul023 <73m6u7h@code.edu.az>
2022-03-14 20:59:22 -0700

    HomeWork

---
+++ HomeWorkC#/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //     int[] array = { 10, 23, 11, 46, 59,14,25,49,21 };
+            //     
+            //     int count = 0;
+            //    int qaliq = 0;
+            //    int sum;
+            //      for (int i = 0; i < array.Length; i++)
+            //     {
+            //          sum = 0;
+            //       while (array[i] >= 1)
+            //         { 
+            //             qaliq = array[i] % 10;
+            //             sum = sum + qaliq;
+            //             array[i] = array[i]  / 10;
+            //         }
+            //         if (sum < 10)
+            //         {
+            //            count++;
+            //           
+            //       }
+            //  Console.WriteLine(count);
+            //          TASK1
+
+            //     }
+
+            //   int count = 0;
+            //   int i ;
+            //   for (i = 1; i <= 8000; i++) 
+            //   {
+            //       if (i % 2 == 0) 
+            //       {
+            //          count++;
+            //       }
+            //   }
+            //   Console.WriteLine(count);
+            // TASK2
+            //   int number = 48;
+            //   int numberFindTool;
+            //   int count = 0;
+            //   for (numberFindTool = 1; numberFindTool * numberFindTool < number; numberFindTool++)
+            //   {
+            //       count++;
+            //   }
+            //   Console.WriteLine(count);
+            // TASK3
+        }
+    }
+} 
