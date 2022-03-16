fd9e2ad07a4005287dbf7d1613bbb6c5c78631bb
Resul023 <73m6u7h@code.edu.az>
2022-03-15 22:11:33 -0700

    Task2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  string name;
+              name = Console.ReadLine();
+              char[] number = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            bool check = true; 
+
+              for (int i = 0; i < name.Length; i++)
+              {
+
+                  for (int j = 0; j < number.Length; j++)
+                  {
+                    if (name[i] == number[j])
+                        check = false;
+                                     
+                  }
+
+              }
+            if (check == true)
+            {
+                Console.WriteLine("There are no number");
+            }
+            else
+            {
+                Console.WriteLine("There are number");
+            }*/ //Task1
+
+            
+                      /*  string name;
+                        name = Console.ReadLine();
+
+                        bool check = true;
+
+
+                             for (int i = 1; i <name.Length; i++)
+                             {
+
+
+
+                            if (name[i - 1] == 'a' && name[i] == 'l')
+                                check = false;
+
+
+
+                             }
+            if (check == true) 
+            {
+                Console.WriteLine("yox");
+            }
+            else
+            {
+                Console.WriteLine("Var");
+            }*/ //Task2
+
+
+            /*         string numberTool = Console.ReadLine();
+                     int number = Convert.ToInt32(numberTool);
+                     int count = 0;
+                     int numberOld=number;
+                     int qaliq = 0;
+                     int x = 10;
+                     while (number >= 1)
+                     {
+                         qaliq = number % 10;
+                         number = number / 10;
+                         count++;
+
+
+                     }
+                     Console.WriteLine(number= numberOld/count );*/ //Task3
+
+        /*    string list = Console.ReadLine();
+            char[] number = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            int count = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                for (int j = 0; j < number.Length; j++)
+                {
+                    if (list[i]==number[j]) 
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+*/  //Task4
+
+
+
+
+
+
+        }
+    }
+}
