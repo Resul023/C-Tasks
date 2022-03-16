38399c2c2efb156056d214b8fb5abdf18efe2e9f
Resul023 <100703183+Resul023@users.noreply.github.com>
2022-03-16 12:25:10 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Task4.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many number will you enter:");
+            string nameSizeTool = Console.ReadLine();
+            int nameSize = Convert.ToInt32(nameSizeTool);
+            
+            string[] names = new string[nameSize];
+             
+            int count = 0;
+            bool check = false;
+            for (int i = 0; i < nameSize; i++)
+            {  
+                names[i] = Console.ReadLine();
+                for (int j = 0 ; j < names[i].Length; j++)
+                {
+                        
+                    switch (names[i][j])
+                     {  
+                            case '0':
+                                count++;
+                                break;
+                            case  '1':
+                                count++;
+                                break;
+                            case '2':
+                                count++;
+                                break;
+                            case '3':
+                                count++;
+                                break;
+                            case '4' :
+                                count++;
+                                break;
+                            case '5' :
+                                count++;
+                                break;
+                            case '6':
+                                count++;
+                                break;
+                            case '7':
+                                count++;
+                                break;
+                            case '8':
+                                count++;
+                                break;
+                            case '9':
+                                count++;
+                                break;
+
+                     }
+                    
+                  
+                }
+                    
+
+
+            }
+            Console.WriteLine(count);
+            
+        }
+        
+    }
+}
